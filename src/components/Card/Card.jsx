d5c43f6293f2b02be1e4aa3ffaddb3e8490edcf0
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-04 19:39:38 +0100

    feat: add grid layout and basic styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import styles from "./Card.module.scss"
 
 export default class Card extends Component {
     render() {
-
         const { name, description, image_url } = this.props.beer;
         return (
-            <>
+            <div className={styles.card}>
                 <img src={image_url} alt={name} />
-
                 <h2>{name}</h2>
                 <p>{description}</p>
-            </>
+            </div>
           );
     }
 }
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import Card from "../Card";
+import styles from "./CardList.module.scss"
 
 export default class CardList extends Component {
 
-    getBeers = () => this.props.beers.map(beer => <Card beer={beer} />);
+    getBeers = () => this.props.beers.map(beer => <Card key={beer.id} beer={beer} />);
 
     render() { 
         return ( 
-            <>
+            <section className={styles.cardList}>
                 {this.getBeers()}
-            </>
+            </section>
           );
     }
 }
