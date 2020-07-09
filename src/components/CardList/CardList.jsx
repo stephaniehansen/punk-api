d0d68120b6273d608c9c1afbb0922ff3b99059ed
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-09 23:48:27 +0100

    refactor: add props destructuring

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -4,12 +4,13 @@ import styles from "./CardList.module.scss"
 
 export default class CardList extends Component {
     getBeers = () => {
-        const checked = this.props.filters.filter(filter => filter.isChecked)
-        .map(filter => filter.value);
+        const { filters, searchTerm } = this.props;
+        const checked = filters.filter(condition => condition.isChecked)
+        .map(condition => condition.value);
         const keep = true;
 
         return this.props.beers
-        .filter(beer => beer.name.toLowerCase().includes(this.props.searchTerm.toLowerCase()))
+        .filter(beer => beer.name.toLowerCase().includes(searchTerm.toLowerCase()))
         .filter(beer => checked.includes("abv") ? beer.abv > 6 : keep)
         .filter(beer => checked.includes("classic") ? beer.first_brewed.slice(-4) < 2010 : keep)
         .filter(beer => checked.includes("acidic") ? beer.ph < 4 : keep)
