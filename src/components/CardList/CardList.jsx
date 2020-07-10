81563d15959f2e20ca503b0250685105bad38956
Stephanie Hansen <stephanie@stephanie.dev>
2020-07-10 10:22:00 +0100

    refactor: simplify ternary

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -7,12 +7,11 @@ export default class CardList extends Component {
         const { filters, beers, searchTerm } = this.props;
         const checked = filters.filter(condition => condition.isChecked)
         .map(condition => condition.value);
-        const keep = true;
 
         return beers.filter(beer => beer.name.toLowerCase().includes(searchTerm.toLowerCase()))
-        .filter(beer => checked.includes("abv") ? beer.abv > 6 : keep)
-        .filter(beer => checked.includes("classic") ? beer.first_brewed.slice(-4) < 2010 : keep)
-        .filter(beer => checked.includes("acidic") ? beer.ph < 4 : keep)
+        .filter(beer => checked.includes("abv") ? beer.abv > 6 : beer)
+        .filter(beer => checked.includes("classic") ? beer.first_brewed.slice(-4) < 2010 : beer)
+        .filter(beer => checked.includes("acidic") ? beer.ph < 4 : beer)
         .map(beer => <Card key={beer.id} beer={beer} />)
     }
 
