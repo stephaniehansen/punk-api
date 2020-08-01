4da5b7ebdf79207b1deb86e1c335b51afaf7a3fa
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-01 23:50:42 +0100

    feat: add logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,6 +8,9 @@ export default class NavBar extends Component {
     render() { 
         return ( 
             <section className={styles.sideNav}>
+                <div class={styles.logo}>
+                    <h1>Brewdog</h1>
+                </div>
                 <SearchBox placeholder="Search..." handleChange={this.props.handleChange} />
                 <FiltersList filters={this.props.filters} handleChecked={this.props.handleChecked}/>
             </section>
