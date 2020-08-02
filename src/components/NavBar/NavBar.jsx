232e951d485d557f85b2e492818cc28df5c91f4d
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 01:13:55 +0100

    feat: add logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from "./NavBar.module.scss"
+import logo from "../../assets/logo.png";
 
 import SearchBox from "../SearchBox";
 import FiltersList from "../FiltersList";
@@ -9,7 +10,7 @@ export default class NavBar extends Component {
         return ( 
             <section className={styles.sideNav}>
                 <div class={styles.logo}>
-                    <h1>Brewdog</h1>
+                    <img src={logo} />
                 </div>
                 <SearchBox placeholder="Search..." handleChange={this.props.handleChange} />
                 <FiltersList filters={this.props.filters} handleChecked={this.props.handleChecked}/>
