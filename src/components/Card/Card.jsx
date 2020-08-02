22a29046bcfb6cd77e5bafb67e47507536c31074
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 02:03:03 +0100

    feat: shorten description

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import styles from "./Card.module.scss"
 
 export default class Card extends Component {
+    shortenDescription = (description) => {
+        return description.length < 190
+            ? description
+            : description.substring(0, description.lastIndexOf(".", 190)) + ".";
+    }
+    
     render() {
-        const { name, description, image_url } = this.props.beer;
+        const { name, description, abv, image_url } = this.props.beer;
         return (
             <div className={styles.card}>
                 <img src={image_url} alt={name} />
-                <h2>{name}</h2>
-                <p>{description}</p>
+                <div class={styles.name}>
+                    <h2>{name.split('-')[0]}</h2>
+                </div>
+                <span>ABV {abv}%</span>
+                <p>{this.shortenDescription(description)}</p>
             </div>
           );
     }
