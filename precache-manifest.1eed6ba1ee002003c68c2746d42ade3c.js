f6d95e6c0bf0ac6f5522aa8606e10dbb5b1c53de
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-02 02:18:25 +0100

    Updates

---
+++ precache-manifest.1eed6ba1ee002003c68c2746d42ade3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d4decc564289d4196e871f2afcee90",
+    "url": "/punk-api/index.html"
+  },
+  {
+    "revision": "db0cd140736f02ecd968",
+    "url": "/punk-api/static/css/main.7c5e8ff4.chunk.css"
+  },
+  {
+    "revision": "87ccdbfd213a8a981c1a",
+    "url": "/punk-api/static/js/2.8f81c9bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/punk-api/static/js/2.8f81c9bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0cd140736f02ecd968",
+    "url": "/punk-api/static/js/main.0a605ff9.chunk.js"
+  },
+  {
+    "revision": "86371ce7990fe1b1cf93",
+    "url": "/punk-api/static/js/runtime-main.ee4e8103.js"
+  }
+]);
