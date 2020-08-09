33877fa0ddd7c861469617f601f5b28cec46d062
Stephanie Hansen <stephanie@stephanie.dev>
2020-08-09 11:40:41 +0100

    feat: add mobile menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,16 +5,37 @@ import logo from "../../assets/logo.png";
 import SearchBox from "../SearchBox";
 import FiltersList from "../FiltersList";
 
-export default class NavBar extends Component {   
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes, faBars } from "@fortawesome/free-solid-svg-icons";
+library.add( faTimes, faBars );
+
+export default class NavBar extends Component {
+    state = {
+        mobileMenu: false
+    }
+
+    toggleMenu = () => {
+        this.setState({ mobileMenu: !this.state.mobileMenu })
+    }
+
+    getIcon = () => {
+        return this.state.mobileMenu ? 
+            <FontAwesomeIcon icon="times" /> : <FontAwesomeIcon icon="bars" />;
+    } 
+
     render() { 
         return ( 
             <section className={styles.sideNav}>
-                <div class={styles.logo}>
-                    <img src={logo} />
+                <span className={styles.mobileMenu} onClick={()=> this.toggleMenu()}>{this.getIcon()}</span>
+                <div className={styles.navContent + (this.state.mobileMenu ? `${styles.show}` : "" )}>
+                    <div class={styles.logo}>
+                        <img src={logo} />
+                    </div>
+                    <SearchBox placeholder="Search..." handleChange={this.props.handleChange} />
+                    <FiltersList filters={this.props.filters} handleChecked={this.props.handleChecked}/>
                 </div>
-                <SearchBox placeholder="Search..." handleChange={this.props.handleChange} />
-                <FiltersList filters={this.props.filters} handleChecked={this.props.handleChecked}/>
             </section>
-         );
+        );
     }
 }
